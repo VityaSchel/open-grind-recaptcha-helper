b4ab515076334ebf6d8d5aabe604223985ad38b5
Viktor Shchelochkov <hi@hloth.dev>
2026-09-17 12:35:17 +0200

    Bump version to v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "org.opengrind.recaptcha"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1000000
-        versionName = "1.0.0"
+        versionCode = 1001000
+        versionName = "1.1.0"
     }
 
     signingConfigs {
